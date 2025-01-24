70f5b8c57a70fa1ecf0d2f16a9cbfd490181f67e
Tyler Nygren <tylernygren@gmail.com>
2025-01-24 18:13:50 -0500

    delineate production config

---
+++ rollup.config.production.js
@@ -0,0 +1,24 @@
+import { readdirSync } from "node:fs";
+import terser from "@rollup/plugin-terser";
+import nodeResolve from "@rollup/plugin-node-resolve"; // needed for material web
+
+const [inputDirectory, outputDirectory] = [`./src/scripts`, `./dist/scripts`];
+
+export default readdirSync(inputDirectory).reduce((accumulator, currentFile) => {
+    if (currentFile.charAt(0) === "_" || currentFile.includes(".mjs") || !currentFile.includes(".js")) {
+        // do not rollup modules/folders
+    } else {
+        accumulator.push({
+            input: `${inputDirectory}/${currentFile}`,
+            output: {
+                file: `${outputDirectory}/${currentFile.split(".")[0]}.min.js`,
+                format: "es"
+            },
+                plugins: [
+                    nodeResolve(),
+                    terser()
+                ]
+        });
+    }
+    return accumulator;
+},[]);
