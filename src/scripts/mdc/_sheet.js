f67c5c044e5c756f19b9054f06543efc7d138c38
Tyler Nygren <tylernygren@gmail.com>
2025-02-03 17:05:21 -0500

    add support for md-sheet

--- src/scripts/mdc/_sheet.js
+++ src/scripts/mdc/_sheet.js
@@ -1,45 +1,146 @@
 import "./_core.js";
-import { motion, Child } from "./_core.js";
+import { motion, Child, breakpoints, Divider } from "./_core.js";
 import { Iconbutton } from "./_iconbutton.js";
 
 class MDSheet extends HTMLElement{
     constructor(){
         super();
     }
+    async addContent(contentArray = []){
+        try{
+            // get sheet content container
+            let sheetContent = this.querySelector(".sheet-content");
+            // fix sheet content container width
+            sheetContent.style.overflow = "hidden";
+            sheetContent.style.width = `${sheetContent.clientWidth}px`;
+            // populate sheet content container with transparent elements
+            for(const item of contentArray){
+                if(item instanceof Child) item.setStyle({opacity:0}).appendTo(sheetContent);
+                if(item instanceof HTMLElement) item.style.opacity = 0; this.append(item);
+            }
+            // animate sheet resize to fit new content width
+            let animation = sheetContent.animate([
+                {width: `${this.clientWidth}px`},
+                {width: `${sheetContent.scrollWidth}px`}
+            ],{
+                duration: motion.duration.short,
+                easing: motion.easing.decelerate,
+                iterations: 1,
+                fill: "forwards"
+            });
+            await animation.finished;
+            animation.commitStyles();
+            animation.cancel();
+            // animate opacity change on all content elements
+            let contentElements = this.querySelectorAll(".sheet-content > *");
+            if(contentElements.length){
+                let i = 0, visibleContentHeight = 0, j = 0;
+                // if lots of elements, only animate the visible ones
+                while(sheetContent.clientHeight > visibleContentHeight && i < contentElements.length){
+                    visibleContentHeight += contentElements[i++].clientHeight;
+                }
+                for(j = 0; j < i; ++j){
+                    animation = sheetContent[j].animate([
+                        {opacity: 0},
+                        {opacity: 1}
+                    ], {
+                        duration: motion.duration.short,
+                        easing: motion.easing.decelerate,
+                        iterations: 1,
+                        fill: "forwards"
+                    });
+                    await animation.finished;
+                    animation.commitStyles();
+                    animation.cancel();
+                }
+                for(j = i; j < contentElements.length; ++j){
+                    sheetContent[j].style.opacity = 1;
+                }
+            }
+            sheetContent.overflow = "scroll";
+        } catch (error) {
+            console.groupCollapsed("Could not add content to <md-sheet>.");
+            console.consoleError(error);
+            console.groupEnd();
+        }
+    }
+    connectedCallback(){
+        slide();
+    }
+    disconnectedCallback(){
+        
+    }
+    async removeContent(){
+        try{
+
+        } catch (error) {
+
+        }
+        // get sheet content container
+        let sheetContent = this.querySelector(".sheet-content");
+        // fade opacity
+        let animation = sheetContent.animate([
+            {opacity: 1},
+            {opacity: 0}
+        ], {
+            duration: motion.duration.short,
+            easing: motion.easing.decelerate,
+            iterations: 1,
+            fill: "forwards"
+        });
+        await animation.finished;
+        animation.commitStyles();
+        animation.cancel();
+        // remove content
+        sheetContent.innerHTML = "";
+        // resize sheet
+        animation = this.animate([
+            {width: `${this.clientWidth}px`},
+            {width: `${sheetContent.scrollWidth}px`}
+        ],{
+            duration: motion.duration.short,
+            easing: motion.easing.decelerate,
+            iterations: 1,
+            fill: "forwards"
+        });
+        await animation.finished;
+        animation.commitStyles();
+        animation.cancel();
+    }
+    replaceContent(contentArray = []){
+        this.removeContent();
+        this.addContent(contentArray);
+    }
 }
 
 export class Sheet extends Child {
-    constructor(headline = "", contentArray = []){
+    constructor(headline = "", contentArray = [], actionsArray = []){
         super();
         this.tag = "md-sheet";
-        this.animation = {
-            slideIn: async function () {
-                let animation = this.animate([
-                    {transform: ""},
-                    {transform: ""}
-                ], {
-                    duration: motion.duration.medium, 
-                    iterations: 1, 
-                    easing: motion.easing.decelerate, 
-                    fill: "forwards"
-                });
-                await animation.finished;
-                animation.commitStyles();
-                animation.cancel();
-            },
-            slideOut: async function () {
-                
-            }
-        }
+        this.styles = {
+            position: "fixed",
+            inset: window.innerWidth < breakpoints.large ? "100svh 0 auto 0" : "0 auto 0 100svw"
+        };
         this.childList = [
             new Child("span")
+                .setClassList("sheet-headline")
                 .setInnerText(headline),
             new Iconbutton("close")
                 .setAriaLabel("close")
-                .setListener("click", this.animation.slideOut),
+                .setClassList("sheet-close")
+                .setListener("click", slide),
             new Child()
+                .setClassList(["sheet-content"])
                 .setChildList(contentArray)
-        ],
+        ];
+        if(actionsArray.length) this.childList.push([
+            new Child()
+                .setClassList(["sheet-actions"])
+                .setChildList([
+                    new Divider(),
+                    ...actionsArray
+                ])
+        ]);
         this.shadowRoot = {
             isAttached: true,
             mode: "closed",
@@ -47,8 +148,23 @@ export class Sheet extends Child {
             childList: [
                 new Child().setClassList(["scrim"])
             ]
-        }
+        };
     }
+}
+async function slide() {
+    let positionStart = this.style.translate ? this.style.translate.split(" ") : ["0%","0%"];
+    let keyframes = [{translate: `${positionStart[0]} ${positionStart[1] != undefined ? positionStart[1] : "0%"}`}];
+    if(this.style.left === "100svw") keyframes.push({translate: `${positionStart[0] === "0%" ? "-100%" : "0%"} 0%`});
+    if(this.style.top === "100svh") keyframes.push({translate: `0% ${positionStart[1] === "0%" ? "-100%" : "0%"}`});
+    let animation = this.animate(keyframes, {
+        duration: motion.duration.medium,
+        easing: motion.easing.decelerate,
+        iterations: 1,
+        fill: "forwards"
+    });
+    await animation.finished;
+    animation.commitStyles();
+    animation.cancel();
 }
 
 customElements.define("md-sheet", MDSheet);
