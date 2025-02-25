593e31001ec66fc5343c6a428e187296ce23b2ef
Tyler Nygren <tylernygren@gmail.com>
2025-02-25 09:38:21 -0500

    delineate camel and kebab case functions

--- src/scripts/_string.js
+++ src/scripts/_string.js
@@ -1,30 +1,28 @@
-export function camelCase(string = "", delimiter = " ") {
-    string = string.split(delimiter);
-    if(string.length > 1) {
-        return string.reduce((accumulator, currentValue, i) => {
-            return i === 0 ? accumulator += currentValue : accumulator += `${currentValue.charAt(0).toUpperCase()}${currentValue.slice(1)}`;
-        }, "");
-    } else {
-        return string.split(/(?=[A-Z])/).reduce((accumulator, currentValue, i, initialArray) => {
-            return i < initialArray.length - 1 ? accumulator += `${toLowerCase(currentValue)}${delimiter}`: accumulator += toLowerCase(currentValue);
-        }, "");
-    }
+export function camelCase(string = "") {
+    return string.split(/-|[A-Z]|_|\s/g).reduce((acc, cv, i) => {
+        return i === 0 ? acc += cv : acc += `${cv.charAt(0).toUpperCase()}${cv.slice(1)}`;
+    }, "");
 }
 export function capitalize(string = "", delimiter = " ") {
-    return string.split(delimiter).reduce((accumulator, currentValue, i, initialArray) => {
+    return string.split(delimiter).reduce((acc, cv, i, initialArray) => {
         return i < initialArray.length - 1 ? 
-            accumulator += `${currentValue.charAt(0).toUpperCase() + currentValue.slice(1)}${delimiter}`: 
-            `${currentValue.charAt(0).toUpperCase() + currentValue.slice(1)}`;
+            acc += `${cv.charAt(0).toUpperCase() + cv.slice(1)}${delimiter}`: 
+            `${cv.charAt(0).toUpperCase() + cv.slice(1)}`;
+    }, "");
+}
+export function kebabCase(string){
+    return string.split(/[A-Z]|_|\s/g).reduce((acc, cv, i, initialArray) => {
+        return i < initialArray.length - 1 ? acc += `${toLowerCase(cv)}-`: acc += toLowerCase(cv);
     }, "");
 }
 export function replaceDelimiter(string = "", splitDelimiter = " ", joinDelimiter = "-", toLowercase = true){
     if(toLowercase) string = string.toLowerCase();
-    return string.split(splitDelimiter).reduce((accumulator, currentValue, i, initialArray) => { 
-            return i < initialArray.length - 1 ? accumulator += `${currentValue}${joinDelimiter}`: accumulator += currentValue; 
+    return string.split(splitDelimiter).reduce((acc, cv, i, initialArray) => { 
+            return i < initialArray.length - 1 ? acc += `${cv}${joinDelimiter}`: acc += cv; 
         }, "" );
 }
 export function splice(string = "", start = 0, deleteCount = 0, item = "") {
-    return [...string].splice(start, deleteCount, item).reduce((accumulator, currentValue) => {
-        return accumulator += currentValue;
+    return [...string].splice(start, deleteCount, item).reduce((acc, cv) => {
+        return acc += cv;
     }, "");
 }
